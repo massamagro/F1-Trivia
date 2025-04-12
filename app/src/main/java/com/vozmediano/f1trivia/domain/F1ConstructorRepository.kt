106e566c8006303168497cde42d9f1f64026912f
Carlos Vozmediano <carlosvozmedianoruiz@gmail.com>
2025-04-12 21:32:18 +0200

    Code cleaning

--- app/src/main/java/com/vozmediano/f1trivia/domain/F1ConstructorRepository.kt
+++ app/src/main/java/com/vozmediano/f1trivia/domain/F1ConstructorRepository.kt
@@ -3,7 +3,6 @@ package com.vozmediano.f1trivia.domain
 import com.vozmediano.f1trivia.domain.model.f1.Constructor
 
 interface F1ConstructorRepository {
-    //CONSTRUCTORS
     suspend fun getConstructors(): List<Constructor>
     suspend fun getConstructorById(constructorId: String): Constructor
     suspend fun getConstructorsBySeason(season: String): List<Constructor>
